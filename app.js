5e953600d7f401bb3137f7e8ec516e1b06e94015
Joe Longstreet <joelongstreet@gmail.com>
2015-01-13 15:55:45 -0600

    write file reader to return example content

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var path = require('path');
+var examples = require('./lib/examples');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
+var exampleContent = [];
+examples.getImages().then(function(content){
+  exampleContent = content;
+});
+
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('index', { examples : exampleContent });
 });
 
 module.exports = app;
