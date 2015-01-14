7621cfc6a92f6f67855edf34213222e444ff4f6b
Joe Longstreet <joelongstreet@gmail.com>
2015-01-13 18:03:12 -0600

    allow users to download zip files

--- app.js
+++ app.js
@@ -7,13 +7,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.get('/', function(req, res){
   res.render('index', { examples : examples.getReferences() });
 });
+
 
 app.get('/example/:directory', function(req, res){
   var ref = examples.getReference(req.params.directory);
   res.render('example', ref);
 });
+
+
+app.get('/zip/:example', function(req, res){
+  examples.generateZip(req.params.example, function(zipFilePath){
+    res.sendfile(zipFilePath);
+  });
+});
+
 
 module.exports = app;
